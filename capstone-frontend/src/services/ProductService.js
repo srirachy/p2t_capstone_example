55e9ba9d0f4202433f643b65602e0fab6d4fc89a
Mr.Gibbs <javier.guerra1001@gmail.com>
2025-08-18 10:07:48 -0700

    feat (get product): added functionality to fetch a product by its id, and added comments to help guide developers to use it

--- capstone-frontend/src/services/ProductService.js
+++ capstone-frontend/src/services/ProductService.js
@@ -6,3 +6,9 @@ export async function getProducts() {
     const { data } = await axios.get(`${baseUrl}products`);
     return data;
 }
+
+// This function retrieves a product by its ID and has been created for you, but you will need to import it in your components as needed.
+export async function getProductById(id) {
+    const { data } = await axios.get(`${baseUrl}products/${id}`);
+    return data;
+}
