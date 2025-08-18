5f9a947616a3f7771b57b3a3bce2514734dfc793
Mr.Gibbs <javier.guerra1001@gmail.com>
2025-08-18 10:02:07 -0700

    feat(cart store): created a cart store using Zustand to help keep track of the items added to the cart by the users

--- capstone-frontend/src/components/Navbar.jsx
+++ capstone-frontend/src/components/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { NavLink } from 'react-router';
+import { useCartStore } from '../store/CartStore';
 
 const Navbar = () => {
+  const { items } = useCartStore();
+  const totalPrice = items?.reduce((total, item) => total + item.price, 0);
   return (
-    <div>Navbar</div>
+    <nav>
+      <h1>Header Goes Here</h1>
+      <section>
+        <NavLink to="/" className='nav-link'>Home</NavLink>
+        {/* You will have to adjust the NavLink element below this comment to create a real checkout page, starting with creating a new route in your project and then adjusting the `to` property */}
+        <NavLink to="#" className='nav-link'>Checkout ${totalPrice}</NavLink>
+        {/* Create New NavLink Elements in your project and place them below this comment */}
+
+        {/* Create New NavLink Elements in your project and place them above this comment */}
+      </section>
+    </nav>
   )
 }
 
